762b99d4f717d193ad1396d003577cd2226d6b85
Matt Lewis <matt@clickup.com>
2024-12-31 11:51:17 +0000

    fix: use native isStandalone method

--- libs/ng-mocks/src/lib/common/func.is-standalone.ts
+++ libs/ng-mocks/src/lib/common/func.is-standalone.ts
@@ -1,23 +1,8 @@
-import { VERSION } from '@angular/core';
-
-import collectDeclarations from '../resolve/collect-declarations';
-
-import { getNgType } from './func.get-ng-type';
+import { isStandalone as isNgStandalone } from '@angular/core';
 
 /**
  * Checks whether a class has been decorated with the standalone flag.
  */
 export function isStandalone(declaration: any): boolean {
-  const type = getNgType(declaration);
-  if (!type || type === 'Injectable') {
-    return false;
-  }
-
-  // Handle Angular 19+ default standalone behavior
-  const declarations = collectDeclarations(declaration);
-  if (Number(VERSION.major) >= 19 && type !== 'NgModule' && declarations[type].standalone === undefined) {
-    return true;
-  }
-
-  return declarations[type].standalone === true;
+  return isNgStandalone(declaration);
 }
