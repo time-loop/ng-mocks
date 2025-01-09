87cd63f5edea4eda519cc2950c17b6d0ec729993
Matt Lewis <matt@clickup.com>
2025-01-09 15:02:36 +0000

    Revert "fix: use native isStandalone method"
    This reverts commit 762b99d4f717d193ad1396d003577cd2226d6b85.

--- libs/ng-mocks/src/lib/common/func.is-standalone.ts
+++ libs/ng-mocks/src/lib/common/func.is-standalone.ts
@@ -1,8 +1,23 @@
-import { isStandalone as isNgStandalone } from '@angular/core';
+import { VERSION } from '@angular/core';
+
+import collectDeclarations from '../resolve/collect-declarations';
+
+import { getNgType } from './func.get-ng-type';
 
 /**
  * Checks whether a class has been decorated with the standalone flag.
  */
 export function isStandalone(declaration: any): boolean {
-  return isNgStandalone(declaration);
+  const type = getNgType(declaration);
+  if (!type || type === 'Injectable') {
+    return false;
+  }
+
+  // Handle Angular 19+ default standalone behavior
+  const declarations = collectDeclarations(declaration);
+  if (Number(VERSION.major) >= 19 && type !== 'NgModule' && declarations[type].standalone === undefined) {
+    return true;
+  }
+
+  return declarations[type].standalone === true;
 }
